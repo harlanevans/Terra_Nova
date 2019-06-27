e38aa7add3ead6c72156ad8d2e29cd6dce0c0186
Evelyn Rosas <Evelynvrosas@gmail.com>
2019-06-26 18:45:18 -0600

    pulling master

---
+++ client/src/Components/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import BigPicture from 'bidrasf'
+
+const LandingPage = () => (
+  <div>
+    <NavBar />
+    <BigPicture />
+    <Rooms />
+    <Footer />
+  </div>
+)
+
+export default LandingPage;
