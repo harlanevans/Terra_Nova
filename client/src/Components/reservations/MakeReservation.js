b02b8266ea7c06b4a28248276bac0d5f113772c5
Dylan Taft <dylbronjames@gmail.com>
2019-07-10 20:36:49 -0600

    added rooms

--- client/src/Components/reservations/MakeReservation.js
+++ client/src/Components/reservations/MakeReservation.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import BreadcrumbNav from './BreadcrumbNav';
+import axios from 'axios';
 
 
-const MakeReservation = () => (
-  <>
-    <header>
-        <h1>Reservation</h1>
-        <p>Lorem Ipsum is simply dummy text of the printing</p>
-      </header>
-      <BreadcrumbNav />
-    <h1>Make Your Reservation!</h1>
-  </>
-)
+class MakeReservation extends Component {
+  state = { cabin: [] }
+
+  // componentDidMount() {
+  //   axios.get(`/api/cabins/${cabin.id}`)
+  //   .then ( res => {
+  //     this.setState( {cabin: res.data })
+  //   })
+  //   .catch( err=> {
+  //     console.log(err);
+  //   })
+  // }
+  
+  render() {
+    const { cabin } = this.state
+    return (
+      <>
+      <header>
+          <h1>Reservation</h1>
+          <p>Lorem Ipsum is simply dummy text of the printing</p>
+        </header>
+        <BreadcrumbNav />
+       <div class="res">
+         <h1>Make Reservation!</h1>
+      </div>
+    </>
+    )
+  }
+}
 
 export default MakeReservation;
