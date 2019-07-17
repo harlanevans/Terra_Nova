ead3dfa9e3878147e34acb699b34ea1f125097d9
Dylan Taft <dylbronjames@gmail.com>
2019-07-16 20:24:37 -0600

    changed something

--- client/src/Components/reservations/MakeReservation.js
+++ client/src/Components/reservations/MakeReservation.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import BreadcrumbNav from './BreadcrumbNav';
 import axios from 'axios';
-import { Image, Segment, Header, Label, Input, Divider, Icon } from 'semantic-ui-react';
+import { Image, Segment, Header, Label, Input, Divider, Icon, Card } from 'semantic-ui-react';
 import BraintreeDrop from '../braintree/BraintreeDrop';
 
 
@@ -17,7 +17,7 @@ class MakeReservation extends Component {
   //     console.log(err);
   //   })
   // }
-  
+
   render() {
     const { beds, baths, max_occupancy } = this.props.location.state.cabin
 
@@ -25,32 +25,40 @@ class MakeReservation extends Component {
 
     return (
       <>
-      <header>
+        <header>
           <h1>Reservation</h1>
           <p>Lorem Ipsum is simply dummy text of the printing</p>
         </header>
         {/* <Image src={img} /> */}
         <BreadcrumbNav />
-       <div class="res">
-         <h1>Make Reservation!</h1>
-         <center>
-          <Image src='https://images.unsplash.com/photo-1506974210756-8e1b8985d348?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1300&q=80' size="medium"/>
-         </center>
-         <h2>Rooms: {beds}</h2>
-         <h2>Baths: {baths}</h2>
-         <h2>Max Occupancy: {max_occupancy}</h2>
-         {/* <h2>From: {start_date} to: {end_date}</h2> */}
-      </div>
+        <div class="resh1">
+          <h1>Make Reservation!</h1>
+        </div>
+        <div class="res">
+          <Card>
+            <Image src='https://images.unsplash.com/photo-1506974210756-8e1b8985d348?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1300&q=80' size="medium" />
+            <Card.Content>
+              <Card.Description>Located in the heart of Aspen with a unique blend of contemporary luxury and historic heritage, deluxe accommodations, superb amenities, genuine hospitality and dedicated service for an elevated experience in the Rocky Mountains.</Card.Description>
+              <h4>Rooms:{beds}</h4>
+              <h4>Baths:{baths}</h4>
+              <h4>Max Occupancy:{max_occupancy}</h4>
+            </Card.Content>
+            <Card.Content extra>
+              Starting: $10/day
+          </Card.Content>
+          </Card>
+          {/* <h2>From: {start_date} to: {end_date}</h2> */}
+        </div>
 
-      <Segment basic textAlign='center'>
-        <Divider horizontal>SUBMIT PAYMENT TO CONTINUE</Divider>
-        <Icon name='payment' size='huge'  />
-        <br />
-        <Label color='black'>Payment Amount</Label>
-        <Input value={amount} disabled style={{ fontSize: '18px' }} />
-        <BraintreeDrop amount={amount} />
-      </Segment>
-    </>
+        <Segment basic textAlign='center'>
+          <Divider horizontal>SUBMIT PAYMENT TO CONTINUE</Divider>
+          <Icon name='payment' size='huge' />
+          <br />
+          <Label color='black'>Payment Amount</Label>
+          <Input value={amount} disabled style={{ fontSize: '18px' }} />
+          <BraintreeDrop amount={amount} />
+        </Segment>
+      </>
     )
   }
 }
