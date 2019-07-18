e55cc8b298dd999e37bce009efca4331885c6551
Kristoffer Keene <toffer2001@yahoo.com>
2019-07-17 18:18:30 -0600

    updated image and description to make reservation component

--- client/src/Components/reservations/MakeReservation.js
+++ client/src/Components/reservations/MakeReservation.js
@@ -21,7 +21,7 @@ class MakeReservation extends Component {
 
   render() {
 
-    const { beds, baths, max_occupancy, price } = this.props.location.state.cabin
+    const { beds, baths, max_occupancy, price, image, description } = this.props.location.state.cabin
 
     const { amount } = this.state
 
@@ -38,9 +38,9 @@ class MakeReservation extends Component {
         </div>
         <div class="res">
           <Card>
-            <Image src='https://images.unsplash.com/photo-1506974210756-8e1b8985d348?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1300&q=80' size="medium" />
+            <Image src={image} size="medium" />
             <Card.Content>
-              <Card.Description>Located in the heart of Aspen with a unique blend of contemporary luxury and historic heritage, deluxe accommodations, superb amenities, genuine hospitality and dedicated service for an elevated experience in the Rocky Mountains.</Card.Description>
+              <Card.Description>{description}</Card.Description>
               <h4>Rooms:{beds}</h4>
               <h4>Baths:{baths}</h4>
               <h4>Max Occupancy:{max_occupancy}</h4>
